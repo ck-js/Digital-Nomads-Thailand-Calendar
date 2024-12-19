6e77c38346faa15819149822689d7c35f76070e1
ck-js <ckentertainmentinfo@gmail.com>
2024-12-19 20:45:45 +0700

    vue initial commit with project setup with npm create vue@latest and using vite build tool with dayjs library to define references for current day month and year and able to render it to parent App.vue component.

---
+++ src/components/Calendar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref, computed} from 'vue'
+import dayjs  from 'dayjs';
+
+const today = ref(dayjs().format('YYYY-MM-DD'));
+const currentMonth = ref(dayjs().format('MMMM'));
+const currentYear = ref(dayjs().format('YYYY'));
+</script>
+<template>
+    <header>
+        <div class="wrapper">
+        <h1>
+            {{ today}}
+            {{ currentMonth}}
+            {{ currentYear}}
+         </h1>
+        </div>
+    </header>
+    
+</template>
